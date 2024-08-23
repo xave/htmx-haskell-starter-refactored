210a7acc49ce056e76cb0a5477b4acda97871a8b
xave <theorem_03pistil@icloud.com>
2024-08-23 15:33:38 -0400

    Add lucid2 code to generate same html as 'form.html'
    - Add all form.html updates to repo
    - Add useHtmx func
    - rename cabal file
    - add packages to cabal file
    - remove unused packages

--- app/Main.hs
+++ app/Main.hs
@@ -8,11 +8,13 @@ import Data.Int (Int64)
 import Data.String (fromString)
 import Data.Text (Text)
 import qualified Data.Text as T
+import qualified Data.Text.Lazy as TL
 import Data.Time
 import Database.SQLite.Simple
 
-import Htmx.Lucid.Core (hxSwap_, hxTarget_)
-import Htmx.Lucid.Extra (hxDelete_)
+import Htmx.Event (HtmxEvent (..))
+import Htmx.Lucid.Core (OnEvent (..), hxGet_, hxOn_, hxPost_, hxPushUrl_, hxSwapOob_, hxSwap_, hxTarget_, hxTrigger_)
+import Htmx.Lucid.Extra (hxConfirm_, hxDelete_, hxPut_)
 
 import Lucid
 import Network.HTTP.Types (
@@ -40,6 +42,7 @@ main :: IO ()
 main = do
     setupDB
     putStrLn $ "Listening on http://localhost:" ++ show port
+    putStrLn $ TL.unpack $ renderText form
     run port $ midLog app -- run :: Port -> Application -> IO ()
   where
     port = 3000
@@ -52,7 +55,8 @@ app req respond = do
         ("GET", ["todos"]) -> getTodos
         ("POST", ["form"]) -> postForm params
         _nonIO -> return $ case mepinf of
-            ("GET", []) -> resHtmlFile "app/form.html"
+            -- ("GET", []) -> resHtmlFile "app/form.html"
+            ("GET", []) -> resHtmlFile "../temphtml.html"
             ("GET", ["htmx"]) -> resJs "app/htmx.min.js"
             _undefinedPath -> errorPage $ rawPath ++ " is undefined" where rawPath = toString $ rawPathInfo req
     respond res
@@ -163,3 +167,50 @@ responseByteString h = responseBuilder status200 textHtml $ Builder.lazyByteStri
 
 resJs :: FilePath -> Response
 resJs = resFile textJs
+
+useHtmx :: (Monad m) => HtmlT m ()
+useHtmx = script_ [defer_ "", src_ htmxSrcPath] ("" :: Html ())
+  where
+    htmxSrcPath = "/htmx"
+
+form :: Html ()
+form =
+    doctypehtml_ $ do
+        head_ $
+            do
+                title_
+                    "goblins"
+                -- Note: For simplicity, you have to write out the entire src= part for scripts. Inconvenient you say? Why are you trying to use so much JS conveniently...?
+                -- script_ "/htmx"
+                -- script_ [defer_ "", src_ "/htmx"] ("" :: Html ())
+                useHtmx
+        body_ $
+            do
+                form_
+                    [hxPost_ "/form", hxTarget_ "#todos", hxSwap_ "beforeend", hxOn_ (HtmxOnEvent AfterRequest) "this.reset()"]
+                    $ do
+                        input_ [type_ "text", name_ "msg"]
+                        input_ [type_ "submit", value_ "Add Todo"]
+                div_ [hxGet_ "/todos", hxTrigger_ "load", hxSwap_ "outerHTML"] ""
+                div_ [hxPost_ "/mouse_entered", hxTrigger_ "mouseenter"] "[Here Mouse, Mouse!]"
+
+                template_
+                    $ do
+                        tr_ [id_ "message", hxSwapOob_ "true"]
+                    $ do
+                        td_ "Joe"
+                        td_ "Smith"
+                        ""
+
+                -- I added this randomly as html exploration
+                button_
+                    [ hxDelete_ "/account"
+                    , hxConfirm_ "Are you sure you wish to delete your account?"
+                    ]
+                    "Delete My Account"
+                div_ [hxConfirm_ "Are you sure?"] $
+                    do
+                        button_ [hxDelete_ "/account"] "Delete My Account"
+                        button_ [hxPut_ "/account"] "Update My Account"
+                        button_ [hxConfirm_ "unset", hxPushUrl_ "true", hxGet_ "/fake_path_FOO"] "Cancel"
+                        ""
